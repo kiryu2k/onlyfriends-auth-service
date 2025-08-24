65f74ba4cf2e909bd7e5bcb9ac9d027abb8f289e
kira kira <sailor.kirill@gmail.com>
2025-08-24 21:53:14 +0300

    fix hasher

--- internal/service/hasher/service.go
+++ internal/service/hasher/service.go
@@ -3,7 +3,10 @@ package hasher
 import (
 	"context"
 	"crypto/rand"
+	"crypto/subtle"
 	"encoding/base64"
+	"fmt"
+	"strings"
 
 	"github.com/pkg/errors"
 	"golang.org/x/crypto/argon2"
@@ -32,16 +35,17 @@ func (s service) Hash(_ context.Context, v string) (string, error) {
 	if err != nil {
 		return "", errors.WithMessage(err, "generate salt")
 	}
-	hash := argon2.IDKey([]byte(v), salt, s.iterCount, s.memory, s.parallel, s.tagLength)
-	return base64.StdEncoding.EncodeToString(hash), nil
+	hash := s.hash(v, salt)
+	return s.joinSaltToHash(hash, salt), nil
 }
 
-func (s service) VerifyHash(ctx context.Context, v string, hash string) (bool, error) {
-	hashToCompare, err := s.Hash(ctx, v)
+func (s service) VerifyHash(ctx context.Context, v string, encodedHash string) (bool, error) {
+	expHash, salt, err := s.splitHashAndSalt(encodedHash)
 	if err != nil {
-		return false, errors.WithMessage(err, "hash")
+		return false, errors.WithMessage(err, "split hash and salt")
 	}
-	return hash == hashToCompare, nil
+	computedHash := s.hash(v, salt)
+	return subtle.ConstantTimeCompare(expHash, computedHash) == 1, nil
 }
 
 func (s service) generateSalt() ([]byte, error) {
@@ -51,4 +55,38 @@ func (s service) generateSalt() ([]byte, error) {
 		return nil, errors.WithMessage(err, "read rand")
 	}
 	return salt, nil
+}
+
+func (s service) hash(v string, salt []byte) []byte {
+	return argon2.IDKey([]byte(v), salt, s.iterCount, s.memory, s.parallel, s.tagLength)
+}
+
+const (
+	sep = "$$"
+)
+
+func (s service) joinSaltToHash(hash []byte, salt []byte) string {
+	return fmt.Sprintf("%s%s%s",
+		base64.StdEncoding.EncodeToString(hash),
+		sep,
+		base64.StdEncoding.EncodeToString(salt),
+	)
+}
+
+func (s service) splitHashAndSalt(encodedHash string) (hash []byte, salt []byte, err error) {
+	hashAndSalt := strings.Split(encodedHash, sep)
+	if len(hashAndSalt) != 2 {
+		return nil, nil, errors.Errorf("unexpected count of encoded hash parts: %d", len(hashAndSalt))
+	}
+
+	hash, err = base64.StdEncoding.DecodeString(hashAndSalt[0])
+	if err != nil {
+		return nil, nil, errors.WithMessage(err, "decode base64 hash")
+	}
+	salt, err = base64.StdEncoding.DecodeString(hashAndSalt[1])
+	if err != nil {
+		return nil, nil, errors.WithMessage(err, "decode base64 salt")
+	}
+
+	return hash, salt, nil
 }
