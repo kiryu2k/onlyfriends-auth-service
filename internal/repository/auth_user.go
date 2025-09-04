785cfe8e82cf61d48f9f26774ed4e7977d76f031
kira kira <sailor.kirill@gmail.com>
2025-09-04 13:51:45 +0300

    add using sqlx

--- internal/repository/auth_user.go
+++ internal/repository/auth_user.go
@@ -4,16 +4,16 @@ import (
 	"context"
 	"database/sql"
 
-	"github.com/jackc/pgx/v5"
+	"github.com/jmoiron/sqlx"
 	"github.com/kiryu2k/onlyfriends-auth-service/internal/entity"
 	"github.com/pkg/errors"
 )
 
 type authUser struct {
-	db *pgx.Conn
+	db *sqlx.DB
 }
 
-func NewAuthUser(db *pgx.Conn) authUser {
+func NewAuthUser(db *sqlx.DB) authUser {
 	return authUser{db: db}
 }
 
@@ -21,7 +21,7 @@ func (r authUser) InsertAuthUser(ctx context.Context, req entity.AuthUser) error
 	query := `
 		INSERT INTO auth_user (id, email, hashed_password, created_at, updated_at)
 		VALUES ($1, $2, $3, $4, $5)`
-	_, err := r.db.Exec(ctx, query, req.Id, req.Email, req.HashedPassword, req.CreatedAt, req.UpdatedAt)
+	_, err := r.db.ExecContext(ctx, query, req.Id, req.Email, req.HashedPassword, req.CreatedAt, req.UpdatedAt)
 	if err != nil {
 		return errors.WithMessagef(err, "exec insert query: '%s'", query)
 	}
@@ -34,7 +34,7 @@ func (r authUser) GetAuthUserByEmail(ctx context.Context, email string) (*entity
 		FROM auth_user
 		WHERE email = $1`
 	u := new(entity.AuthUser)
-	err := r.db.QueryRow(ctx, query, email).Scan(u)
+	err := r.db.QueryRowContext(ctx, query, email).Scan(u)
 	switch {
 	case errors.Is(err, sql.ErrNoRows):
 		return nil, errors.WithMessagef(entity.ErrUserNotFound, "select query row: '%s'", query)
